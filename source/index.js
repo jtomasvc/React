fb07c7d493398f69f8642a5065c5d787cb6aa2c1
nicolas vergara cifuenteso <nicolas.vergara@infotrack.com.co>
2020-05-07 15:45:06 -0500

    creacion del App donde se organizan los componentes

--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './Components/HelloWorld.jsx';
+/*import HelloWorld from './Components/HelloWorld.jsx';*/
+import App from './Containers/App'
 
-ReactDOM.render(<HelloWorld/>,document.getElementById('app'));
+ReactDOM.render(<App/>,document.getElementById('app'));
