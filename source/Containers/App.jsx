af8b02fab23d8b7e79d16097cadbbb25d23ad5fb
nicolas vergara cifuenteso <nicolas.vergara@infotrack.com.co>
2020-05-11 12:50:48 -0500

    se agrego el Fake API y se trabajo con esta

--- source/Containers/App.jsx
+++ source/Containers/App.jsx
@@ -1,53 +1,51 @@
-import React from 'react'; 
+import React, {useState,useEffect} from 'react'; 
 import Header from '../Components/Header';
 import Search from '../Components/Search';
 import Categorias from '../Components/Categorias';
 import Carousel from '../Components/Carousel';
 import CarouselItem from '../Components/Carousel-item';
 import Footer from '../Components/Footer';
+import UseInitialState from '../Hooks/UseInitialState';
 
 import '../assets/styles/App.scss';
+import useInitialState from '../Hooks/UseInitialState';
 
-const App = () => (
+ const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+ const initalState = useInitialState(API);
+    return (
     <div className="App">
         <Header/>
         <Search/>
+
+        {initalState.mylist.length > 0 &&
         <Categorias title="Mi Lista">
             <Carousel>
                 <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
             </Carousel>
-        </Categorias>
+        </Categorias>       
+         }
 
         <Categorias title="Tendencias">
             <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
+            {initalState.trends.map(item =>
+                <CarouselItem key={item.id} {...item}/>
+            )}
             </Carousel>
         </Categorias>
 
         <Categorias title="Originales">
             <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
+            {initalState.originals.map(item =>
+                <CarouselItem key={item.id} {...item}/>
+            )}
             </Carousel>
         </Categorias>
          
         <Footer/>
 
     </div>
-);
-
+  );
+}
 export default App;
